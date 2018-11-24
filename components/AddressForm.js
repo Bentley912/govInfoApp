6e33436e0007b9e167f4715c59563116520501bf
Patrick Bentley <pbentley07@gmail.com>
2018-11-24 17:35:41 +0300

    Added Spinner for API Call Async

--- components/AddressForm.js
+++ components/AddressForm.js
@@ -8,9 +8,9 @@ import {
     FormLabel, 
     FormInput, 
     Button,
-  } from 'react-native-elements'
+} from 'react-native-elements'
 
-  import InfoButton from './InfoButton';
+import InfoButton from './InfoButton';
 
   export default class AddressForm extends React.Component {
     constructor(props){
@@ -20,6 +20,7 @@ import {
           city: '',
           state: '',
           zip: '',
+          isFetching: false,
         };
     }
 
@@ -35,44 +36,37 @@ import {
     handleZip = (text) => {
         this.setState({ zip: text })
     }
-
-    _getVoterInfo(){
-        // fetch('https://www.googleapis.com/civicinfo/v2/representatives?key=' + this.props.API_KEY + '&address='  + state.line1 + '%20' + state.city + '%20' + state.state + '%20' + state.zip)
-        // .then(function(data){
-        //   results = data.json().then(function(data){
-        //     console.log(data.offices);
-        //   });
-         
-        // })
-        // .catch(function(error){
-        //   console.log(error);
-				// })                   
-    }
     
     render() {
         return (
             <View style={styles.getStartedContainer}>
-
-
-            <FormLabel>Address Line 1</FormLabel>
-            <FormInput 
-            	onChangeText={this.handleLine1}
-            />
-            <FormLabel>City</FormLabel>
-            <FormInput 
-			    onChangeText={this.handleCity}
-            />
-            <FormLabel>State</FormLabel>
-            <FormInput 
-				onChangeText={this.handleState}
-			/>
-            <FormLabel>Zip</FormLabel>
-            <FormInput 
-				onChangeText={this.handleZip}
-			/>
-            
-            <InfoButton info={this.state} API_KEY={this.props.API_KEY} />
-          </View>
+                <FormLabel>Address Line 1</FormLabel>
+                <FormInput 
+                    onChangeText={this.handleLine1}
+                />
+                <FormLabel>City</FormLabel>
+                <FormInput 
+                    onChangeText={this.handleCity}
+                />
+                <FormLabel>State</FormLabel>
+                <FormInput 
+                    onChangeText={this.handleState}
+                />
+                <FormLabel>Zip</FormLabel>
+                <FormInput 
+                    onChangeText={this.handleZip}
+                />
+                <InfoButton 
+                    info={this.state} 
+                    API_KEY={this.props.API_KEY} 
+                    onFetchStart={() => {
+                        this.setState({isFetching: true});
+                      }}
+                      onFetchEnd={() => {
+                        this.setState({isFetching: false});
+                      }}
+                />
+            </View>
         )
     }
   }
@@ -83,5 +77,5 @@ import {
         color: 'rgba(96,100,109, 1)',
         lineHeight: 24,
         textAlign: 'center',
-      },
+    },
   })
--- components/InfoButton.js
+++ components/InfoButton.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+	View,
+	ActivityIndicator,
+} from 'react-native'
+
 import { 
     Button,
 } from 'react-native-elements'
@@ -7,47 +12,55 @@ import {
 export default class InfoButton extends React.Component {
     constructor(props){
 		super(props);
-		this.state = {
-			line1:'',
-		}
 		this.getVoterInfo = this.getVoterInfo.bind(this);
     }
 
 	getVoterInfo(){
+		this.setState({ isFetching: true});
+		this.props.onFetchStart();
 		fetch('https://www.googleapis.com/civicinfo/v2/representatives?key=' + API_KEY + '&address='  + newLine1 + '%20' + newCity + '%20' + newState + '%20')
-		.then(function(data){
-			results = data.json().then(function(data){
-			console.log(data);
+		.then((data) => {
+			results = data.json()
+		.then((data) => {
+			this.setState({data, isFetching:false});
+			this.props.onFetchEnd();
+			console.log(this.state.data);
+			console.log('Ended Fetch:' + this.props.isFetching);
 			});
-		
 		})
-		.catch(function(error){
+		.catch((error) => {
 			console.log(error);
 		})  
-		console.log('Request Url:' + ('https://www.googleapis.com/civicinfo/v2/representatives?key=' + API_KEY + '&address='  + newLine1 + '%20' + newCity + '%20' + newState + '%20'))
 	}
 
-		componentDidMount(){
-			console.log(this.props);
-			API_KEY = this.props.API_KEY;
-		}
+	componentDidMount(){
+		console.log(this.state);
+		API_KEY = this.props.API_KEY;
+	}
 
-		componentDidUpdate(){
-			//Logic adds '%20' to spaces in address fields in order to correctly query the API correctly
-			newLine1 = (this.props.info.line1.split(' ').join('%20'));
-			newCity = (this.props.info.city.split(' ').join('%20'));
-			newState = (this.props.info.state.split(' ').join('%20'));
-			console.log(newLine1);
-		}
+	componentDidUpdate(){
+		//Logic adds '%20' in place of spaces in address fields in order to correctly query the API 
+		newLine1 = (this.props.info.line1.split(' ').join('%20'));
+		newCity = (this.props.info.city.split(' ').join('%20'));
+		newState = (this.props.info.state.split(' ').join('%20'));
+		// console.log(newLine1);
+	}
+
 
     render() {
+		const myButton = 
+		<Button
+			raised
+			icon={{name: 'cached'}}
+			title="Get Info"
+			onPress={this.getVoterInfo}
+		/>
+        const spinner = <ActivityIndicator size="large" color="#0000ff" />
+
         return (
-          <Button
-            raised
-            icon={{name: 'cached'}}
-            title="Get Info"
-            onPress={this.getVoterInfo}
-        	/>
+			<View>
+			{this.props.isFetching ? spinner : myButton}
+			</View>
         )
     }
 
